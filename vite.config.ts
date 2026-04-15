d336860f2b720906a6264f2681746269cb8e092a
Francis Santiago <>
2026-04-15 07:27:16 -0300

    :recyle: Atualizado o caminho do arquivo 404.html no script de build:pages e ajustado o diretório de saída da build para "dist/public"

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   base: process.env.VITE_BASE || "/",
   plugins: [tailwindcss()],
   build: {
-    outDir: "dist",
+    outDir: "dist/public",
     emptyOutDir: true
   },
   server: {
